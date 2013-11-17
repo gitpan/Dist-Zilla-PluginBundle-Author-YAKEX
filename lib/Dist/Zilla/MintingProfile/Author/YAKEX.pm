3261cd058c2d541a6378e8d75e77d143683de1a0
Yasutaka ATARASHI (新 康孝) <yakex@cpan.org>
2013-11-17 04:13:04 +0000

    Import of YAKEX/Dist-Zilla-PluginBundle-Author-YAKEX-v0.1.2 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-Author-YAKEX
    gitpan-cpan-version:      v0.1.2
    gitpan-cpan-path:         YAKEX/Dist-Zilla-PluginBundle-Author-YAKEX-v0.1.2.tar.gz
    gitpan-cpan-author:       YAKEX
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/MintingProfile/Author/YAKEX.pm
+++ lib/Dist/Zilla/MintingProfile/Author/YAKEX.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 # ABSTRACT: Dist::Zilla minting profile the way YAKEX does it
-our $VERSION = 'v0.1.1'; # VERSION
+our $VERSION = 'v0.1.2'; # VERSION
 
 use Moose;
 with 'Dist::Zilla::Role::MintingProfile::ShareDir';
@@ -24,7 +24,7 @@ Dist::Zilla::MintingProfile::Author::YAKEX - Dist::Zilla minting profile the way
 
 =head1 VERSION
 
-version v0.1.1
+version v0.1.2
 
 =head1 SYNOPSIS
 
